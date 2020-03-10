4b40574c8dead7d9b1b7bd888bdda8fb5eecda8a
koval.a <anna.koval@nixsolutions.com>
2020-03-10 15:28:09 +0200

    adding OOP task

--- 1-Programming/src/main/java/typeConversions/Exercise3.java
+++
@@ -1,9 +0,0 @@
-package typeConversions;
-
-public class Exercise3 {
-    public static void main(String[] args) {
-        double n = 8.202;
-        int rest = (int) Math.round(n);
-        System.out.println(rest);
-    }
-}
